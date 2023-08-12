cafd0e209859efbee777a9813d27664b0dea5215
bertrandsimon <trandber@hotmail.com>
2023-08-12 15:34:43 +0200

    Cart provider configuration

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,27 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 
+import { CartProvider } from "use-shopping-cart";
+
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    
+    <CartProvider
+    mode="payment"
+    cartMode="client-only"
+    stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}
+    successUrl={`${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}/success`}
+    cancelUrl={`${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}/?success=false`}
+    currency="GBP"
+    allowedCountries={['US', 'GB', 'CA', 'FR']}
+    // enables local storage
+    shouldPersist={true}
+  >
+    
+  
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CartProvider>
   );
 }
